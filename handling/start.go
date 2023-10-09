392af3d2e8400c2e08729ccdaecefeb88089a76c
fredo <sausse.sylvain88@gmail.com>
2023-10-09 17:48:40 +0200

    added cross start of wetty

--- handling/start.go
+++ handling/start.go
@@ -62,7 +62,8 @@ func StartDocker(resp http.ResponseWriter,req *http.Request) {
 		return
 	}
 
-	cmd := exec.Command("screen","-dmS",info.Name+"ssh","cd /home/admin/wetty && node . --ssh-host=localhost --ssh-port=10022 --port=10000")
+	cmd := exec.Command("screen","-dmS",info.Name+"ssh","node . --ssh-host=localhost --ssh-port=10022 --port=10000")
+	cmd.Dir = "/home/admin/wetty"
 	if err := cmd.Run(); err != nil {
 		log.Default().Println(err.Error())
 		resp.WriteHeader(http.StatusPreconditionFailed)
